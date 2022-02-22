e4d3616355e8b267c947c100ce4212ad2e71ab11
Jesus Sistos <jesussistos.josb@gmail.com>
2022-02-22 00:30:49 -0500

    Solved games 200

--- Coding_Challenges/games_200_CHSH_template/CHSH_game_template.py
+++ Coding_Challenges/games_200_CHSH_template/CHSH_game_template.py
@@ -19,6 +19,8 @@ def prepare_entangled(alpha, beta):
 
     # QHACK #
 
+    init_state = [alpha, 0, 0, beta]
+    qml.AmplitudeEmbedding(init_state, wires=[0,1], normalize=True)
     # QHACK #
 
 @qml.qnode(dev)
@@ -42,6 +44,18 @@ def chsh_circuit(theta_A0, theta_A1, theta_B0, theta_B1, x, y, alpha, beta):
     prepare_entangled(alpha, beta)
 
     # QHACK #
+    def rotate_basis(angle, wire):
+        qml.RY(-2*angle, wires=wire)
+
+    if x == 0:
+        rotate_basis(theta_A0, 0)
+    else:
+        rotate_basis(theta_A1, 0)
+
+    if y == 0:
+        rotate_basis(theta_B0, 1)
+    else:
+        rotate_basis(theta_B1, 1)
 
     # QHACK #
 
@@ -61,7 +75,25 @@ def winning_prob(params, alpha, beta):
     """
 
     # QHACK #
+    # x = 0, y = 0 is won trough 00 or 11
+    probs = chsh_circuit(params[0], params[1], params[2], params[3], 0 , 0, alpha, beta)
+    win_00 = probs[0] + probs[3]
 
+    # x = 0, y = 1 is won either through 00 or 11
+    probs = chsh_circuit(params[0], params[1], params[2], params[3], 0 , 1, alpha, beta)
+    win_01 = probs[0] + probs[3]
+
+    # x = 0, y = 1 is won either through 00 or 11
+    probs = chsh_circuit(params[0], params[1], params[2], params[3], 1 , 0, alpha, beta)
+    win_10 = probs[0] + probs[3]
+
+    # x = 1, y = 1, is won either through 01 0r 10
+    probs = chsh_circuit(params[0], params[1], params[2], params[3], 1 , 1, alpha, beta)
+    win_11 = probs[1] + probs[2]
+
+    #Each case has 1/4 probability of being chose.
+    prob = (win_00 + win_01 + win_10 + win_11)/4.
+    return prob
     # QHACK #
     
 
@@ -78,24 +110,25 @@ def optimize(alpha, beta):
 
     def cost(params):
         """Define a cost function that only depends on params, given alpha and beta fixed"""
+        return (1 - winning_prob(params, alpha, beta))**2
 
     # QHACK #
 
     #Initialize parameters, choose an optimization method and number of steps
-    init_params = 
-    opt =
-    steps =
+    pi = np.pi
+    #init_params = np.array([pi/2, pi/2, pi/2, pi/2], requires_grad = True)
+    opt = qml.GradientDescentOptimizer(0.3)
+    steps = 250
 
     # QHACK #
     
     # set the initial parameter values
-    params = init_params
+    params = np.array([pi/4, -pi/4, pi/4, 0], requires_grad = True)
 
     for i in range(steps):
         # update the circuit parameters 
         # QHACK #
-
-        params = 
+        params = opt.step(cost, params)
 
         # QHACK #
 
